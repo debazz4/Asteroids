8191f3a43a3ad8711a59e1183c634db50afb445b
adebola <adebolaajewole@gmail.com>
2024-10-25 06:17:22 +0100

    C: Asteroids and collisions are checked

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,7 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def chk_collision(self, object):
+        return self.position.distance_to(object.position) <= self.radius + object.radius
+        
--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from circleshape import CircleShape
+from shot import Shot
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,9 +31,20 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        for player in updatable:
+            player.update(dt)
+        #player.update(dt)
 
+        for obj in asteroids:
+            if (obj.chk_collision(player)):
+                print("Game over!")
+                sys.exit()
         screen.fill("black")
-        player.draw(screen)
+
+        for player in drawable:
+            player.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
